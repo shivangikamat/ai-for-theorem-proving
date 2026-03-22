cef71ca1fbdaa4a2142f1096633a1c1c0479c652
Shivangi Kamat <kamatpr5@gmail.com>
2026-03-22 23:20:59 +0800

    Initial commit - Lean research project

---
+++ LeanResearch.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LeanResearch` library.
+-- Import modules here that should be built as part of the library.
+import LeanResearch.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanResearch
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
