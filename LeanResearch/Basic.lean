ab1fa26d6e858eb75d5aa5dff9b5cd74cb0f1b85
Shivangi Kamat <kamatpr5@gmail.com>
2026-03-22 23:39:41 +0800

    Some basics

--- LeanResearch/Basic.lean
+++ LeanResearch/Basic.lean
@@ -1,1 +1,28 @@
 def hello := "world"
+
+
+/- Define some constants. -/
+
+def m : Nat := 1
+def n : Nat := 0
+def b1 : Bool := true
+def b2 : Bool := false
+
+/- Check their types. -/
+
+#check m
+#check n
+#check n + 0
+#check m * (n + 0)
+#check b1
+#check b1 && b2
+#check b1 || b2
+#check true
+
+/- Evaluate -/
+
+#eval 5 * 4
+#eval m + 2
+#eval b1 && b2
+
+#check List
