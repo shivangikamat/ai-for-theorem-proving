0285bcb64827aac3529532baab5ee6b0fb52c0b3
Shivangi Kamat <kamatpr5@gmail.com>
2026-03-26 15:17:43 +0800

    Added basics

--- LeanResearch/Basic.lean
+++ LeanResearch/Basic.lean
@@ -28,3 +28,35 @@ def b2 : Bool := false
 #check List
 
 #check let y := 2 + 2; let z := y + y; z * z#eval  let y := 2 + 2; let z := y + y; z * z
+
+open Classical
+
+-- distributivity
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+  Iff.intro
+    (fun h : p ∧ (q ∨ r) =>
+      have hp : p := h.left
+      Or.elim (h.right)
+        (fun hq : q =>
+          show (p ∧ q) ∨ (p ∧ r) from Or.inl ⟨hp, hq⟩)
+        (fun hr : r =>
+          show (p ∧ q) ∨ (p ∧ r) from Or.inr ⟨hp, hr⟩))
+    (fun h : (p ∧ q) ∨ (p ∧ r) =>
+      Or.elim h
+        (fun hpq : p ∧ q =>
+          have hp : p := hpq.left
+          have hq : q := hpq.right
+          show p ∧ (q ∨ r) from ⟨hp, Or.inl hq⟩)
+        (fun hpr : p ∧ r =>
+          have hp : p := hpr.left
+          have hr : r := hpr.right
+          show p ∧ (q ∨ r) from ⟨hp, Or.inr hr⟩))
+
+-- an example that requires classical reasoning
+example (p q : Prop) : ¬(p ∧ ¬q) → (p → q) :=
+  fun h : ¬(p ∧ ¬q) =>
+  fun hp : p =>
+  show q from
+    Or.elim (em q)
+      (fun hq : q => hq)
+      (fun hnq : ¬q => absurd (And.intro hp hnq) h)
